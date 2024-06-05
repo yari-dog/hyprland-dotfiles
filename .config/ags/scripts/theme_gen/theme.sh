4bb3229162ab503d19ed3676197ce9d63f06fadb
yari <yari_dog@protonmail.com>
2024-06-05 20:23:31 +0100

    groundwork for theme generation using pywal

--- .config/ags/scripts/theme_gen/theme.sh
+++ .config/ags/scripts/theme_gen/theme.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 XDG_CACHE_HOME="$HOME"/.cache
+XDG_STATE_HOME="$HOME"/.local/state
 CACHE_DIR="$XDG_CACHE_HOME"/ags/user
 CONFIG_DIR="$HOME"/.config/ags
-SCRIPT_DIR="$HOME"/.config/ags/scripts
+SCRIPT_DIR="$CONFIG_DIR"/scripts
 STATE_DIR="$XDG_STATE_HOME"/ags
 
 IMG=$(swww query | awk -F 'image: ' '{print $2}' | head -n 1)
 
+echo "$IMG"
+
 wal -c
 
-wal -i "$IMG" -q -n -t -v -e --backend haishoku
+wal -i "$IMG" -n -e --backend haishoku
 
 cp "$HOME"/.cache/wal/colors.scss "$CACHE_DIR"/generated/colors.scss
 
-cat "$CONFIG_DIR"/theme_gen/pywal_to_material.scss  >> "$CACHE_DIR"/generated/colors.scss
+cat "$SCRIPT_DIR"/theme_gen/pywal_to_material.scss  >> "$CACHE_DIR"/generated/colors.scss
 
-sass -I "$STATE_DIR/scss" -I "$CONFIG_DIR/scss/fallback" "$CACHE_DIR/user/generated/colors.scss" "$CACHE_DIR/user/generated/colors_classes.scss" --style compressed
-
-cp "$CACHE_DIR"/user/generated/colors_classes.scss "$STATE_DIR"/scss/_material.scss
+cp "$CACHE_DIR"/generated/colors.scss "$STATE_DIR"/scss/_material.scss
 
 # ags
-sass -I "$STATE_DIR/scss" -I "$CONFIG_DIR/scss" "$CONFIG_DIR/scss/main.scss" "$CACHE_DIR/user/generated/style.css"
-ags run-js "App.resetCss(); App.applyCss('${CACHE_DIR}/user/generated/style.css');"
+sass -I "$STATE_DIR/scss" -I "$CONFIG_DIR/scss" "$CONFIG_DIR/scss/main.scss" "$CACHE_DIR/generated/style.css"
+ags run-js "App.resetCss(); App.applyCss('${CACHE_DIR}/generated/style.css');"
