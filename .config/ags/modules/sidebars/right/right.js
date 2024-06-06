7a15ff00d7532506a2ac1c83fbb9c6666f29fac3
yari <yari_dog@protonmail.com>
2024-06-06 16:06:33 +0100

    added txt class to notification labels for fonts

--- .config/ags/modules/sidebars/right/right.js
+++ .config/ags/modules/sidebars/right/right.js
@@ -3,8 +3,76 @@ import MaterialIcon from '../../.common/widgets/MaterialIcon.js';
 import notificationService from '../../../services/notification_service.js';
 const notifications = notificationService.getHandler(false);
 const hyprland = await Service.import('hyprland');
+const mpris = await Service.import('mpris');
+const audio = await Service.import('audio');
 const { Gdk, Gtk } = imports.gi;
 
+
+function Volume(device = 'speaker') {
+    const volume_slider = Widget.Slider({
+	class_name: 'control-panel-volume-slider volume-slider',
+	hexpand: true,
+	min: 0, max: 1,
+	hpack: 'fill',
+	drawValue: false,
+	onChange: ({value}) => {
+	    audio[device].volume = value;
+	},
+	setup: (self) => {
+	    self.hook(audio, (self) => {
+		self.value = audio[device].volume || 0;
+	    });
+	},
+    });
+
+    const volume_mute_button = Widget.Button({
+	class_name: 'control-panel-volume-mute-button',
+	cursor: 'pointer',
+	on_clicked: (self) => {
+	    audio[device].is_muted = !audio[device].is_muted;
+	},
+	setup: (self) => {
+	    self.hook(audio, (self) => {
+		// do some wiggling for the icon name, mic is mic or mic_off, speaker is volume_up or volume_off
+		const muted = audio[device].is_muted;
+		let icon;
+		if (device === 'microphone') {
+		    icon = muted ? 'mic_off' : 'mic';
+		} else {
+		    icon = muted ? 'volume_off' : 'volume_up';
+		}
+		self.child.label = icon;
+	    });
+	},
+	child: MaterialIcon('', 'large', {}),
+    });
+	    
+    return Widget.Box({
+	class_name: 'control-panel-volume',
+	children: [
+	    volume_mute_button,
+	    volume_slider,
+	],
+    });
+}
+
+function ControlPanel() {
+    const volume = Widget.Box({
+	class_name: 'control-panel-volume control-panel-section',
+	spacing: 10,
+	children: [
+	    Volume('speaker'),
+	    Volume('microphone'),
+	],
+    });
+
+    return Widget.Box({
+	class_name: 'control-panel sidebar-section',
+	children: [
+	    volume,
+	],
+    });
+}
 
 function Notifications() {
     const clear_button = Widget.Button({
@@ -160,6 +228,7 @@ export default () => {
 	homogeneous: false,
 	children: [
 	    QuickSettings(),
+	    ControlPanel(),
 	    Notifications(),
 	],
     });
