7b31856f74d3ebd39a1c0a61409e69ff53257658
yari <yari_dog@protonmail.com>
2024-09-06 03:07:01 +0100

    cleaned up css; fixed chat box auto hide

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "rust_analyzer" },
+				ensure_installed = { "lua_ls", "clangd", "rust_analyzer", "html", "cssls" },
 			})
 		end,
 	},
